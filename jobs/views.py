f387e902284468a84ad82b6ab05b7ccc6bd9f897
Jason <jason10wtsmc@hotmail.com>
2020-08-24 23:13:04 +0800

    Created blogs

--- jobs/views.py
+++ jobs/views.py
@@ -1,4 +1,7 @@
 from django.shortcuts import render
 
+from .models import Job #import class named Job from models.py
+
 def home(request):
-    return render(request, 'jobs/home.html')
+    jobs = Job.objects
+    return render(request, 'jobs/home.html', {'jobs':jobs}) #jobs is reference for html
